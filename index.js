ddd7e99067464c0136c11195c491f6f596b3e542
WellCod <weslleygoncalves18@gmail.com>
2022-12-02 21:28:48 -0300

    Primeira versão do site segunda tentativa

---
+++ index.js
@@ -0,0 +1,11 @@
+const botaoMenu = document.querySelector('.cabecalho__menu')
+const menu = document.querySelector('.menu-lateral')
+
+
+botaoMenu.addEventListener('click', () => {
+    menu.classList.toggle('menu-lateral--ativo')
+})
+
+
+
+
